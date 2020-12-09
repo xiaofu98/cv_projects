be81ca200069a94495c60016bebecaa52ee09038
xiaofu98 <728795603@qq.com>
2020-12-09 18:24:27 +0800

    reshape

---
+++ face_recognition/AttendanceProject.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @File    : AttendanceProject.py
+
+import cv2
+import numpy as np
+import face_recognition
+
--- face_recognition/face_basic.py
+++ face_recognition/face_basic.py
@@ -1,33 +1,34 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 # @File    : face_basic.py
-import cv2
-import numpy as np
 import os
+
+import cv2
 import face_recognition
 
 os.makedirs('faces', exist_ok=True)
 
-imgElon = face_recognition.load_image_file('faces/Elon-Musk.jpg')
-imgElon = cv2.cvtColor(imgElon, cv2.COLOR_BGR2RGB)
 
-imgTest = face_recognition.load_image_file('faces/Elon-Test.jpg')
-imgTest = cv2.cvtColor(imgTest, cv2.COLOR_BGR2RGB)
+def convertImg(path):
+    img = face_recognition.load_image_file(path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    return img
 
-imgGates = face_recognition.load_image_file('faces/gates.jpg')
-imgGates = cv2.cvtColor(imgGates, cv2.COLOR_BGR2RGB)
 
-faceLoc = face_recognition.face_locations(imgElon)[0]
-encodeElon = face_recognition.face_encodings(imgElon)[0]
-cv2.rectangle(imgElon, (faceLoc[3], faceLoc[0]), (faceLoc[1], faceLoc[2]), (0, 0, 255), 2)
+def getFace(img):
+    faceLoc = face_recognition.face_locations(img)[0]
+    encode = face_recognition.face_encodings(img)[0]
+    cv2.rectangle(img, (faceLoc[3], faceLoc[0]), (faceLoc[1], faceLoc[2]), (0, 0, 255), 2)
+    return encode
 
-faceLocTest = face_recognition.face_locations(imgTest)[0]
-encodeElonTest = face_recognition.face_encodings(imgTest)[0]
-cv2.rectangle(imgTest, (faceLocTest[3], faceLocTest[0]), (faceLocTest[1], faceLocTest[2]), (0, 0, 255), 2)
 
-faceLocGates = face_recognition.face_locations(imgGates)[0]
-encodeGates = face_recognition.face_encodings(imgGates)[0]
-cv2.rectangle(imgGates, (faceLocGates[3], faceLocGates[0]), (faceLocGates[1], faceLocGates[2]), (0, 0, 255), 2)
+imgElon = convertImg('faces/Elon-Musk.jpg')
+imgTest = convertImg('faces/Elon-Test.jpg')
+imgGates = convertImg('faces/gates.jpg')
+
+encodeElon = getFace(imgElon)
+encodeElonTest = getFace(imgTest)
+encodeGates = getFace(imgGates)
 
 results = face_recognition.compare_faces([encodeElon, encodeGates], encodeElonTest)
 faceDis = face_recognition.face_distance([encodeElon, encodeGates], encodeElonTest)
